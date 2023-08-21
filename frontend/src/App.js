12416f28ee881f2b07725372e3afe362dd6569ab
AliIkmeil <ikmeil.ali@gmail.com>
2023-08-21 23:44:25 +0300

    Added the folders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,16 @@ import { useState,useEffect,createContext } from "react";
 export const tokenContext =createContext()
 function App() {
   const [token, setToken] = useState("")
+  const [isLoggedIn, setIsLoggedIn] = useState("")
   return (
-    <tokenContext.Provider value={{token,setToken}}>
+    <tokenContext.Provider value={{token,setToken,isLoggedIn,setIsLoggedIn}}>
 
     <div className="App">
       <h1>Khalek Bdarak SuperMarker</h1>
       
       
       <Routes>
-      
+
       </Routes>
     </div>
     </tokenContext.Provider>
