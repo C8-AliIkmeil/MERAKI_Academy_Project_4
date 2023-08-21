dd1116a99303a6de052de3ed8f8fc0d11eb81dad
AliIkmeil <ikmeil.ali@gmail.com>
2023-08-21 23:00:34 +0300

    cart done

--- backend/controllers/cartControllers.js
+++ backend/controllers/cartControllers.js
@@ -22,8 +22,26 @@ const addProductsToCart =(req,res)=>{
         })
     })
 }
+const getAllCartProducts = (req,res)=>{
+    // console.log(req.token);
+    // const userId = req.token.userId
+    cartModel.find().exec().then((response)=>{
+        res.status(201).json({
+            success:true,
+            message:"Here is your cart products",
+            products: response
+        })
+    })
+    .catch((err)=>{
+        res.status(500).json({
+            success:false,
+            message:"Something went wrong kindly refresh your page and try again"
+        })
+        })
+}
 
 
 
+module.exports={addProductsToCart,getAllCartProducts}
 
-module.exports={addProductsToCart}
+
--- backend/routes/cartRouters.js
+++ backend/routes/cartRouters.js
@@ -1,7 +1,7 @@
 const express =require("express")
-const { addProductsToCart } = require("../controllers/cartControllers")
+const { addProductsToCart,getAllCartProducts } = require("../controllers/cartControllers")
 const cartRouter = express.Router()
 
-
+cartRouter.get("/",getAllCartProducts)
 cartRouter.post("/",addProductsToCart)
 module.exports=cartRouter
